25836f391fdc872abdd2982b2af2059c7fb29e4b
dylan@dylanhulbert.com <dylan@dylanhulbert.com>
2020-03-27 20:47:03 -0700

    added template files

--- GoodReadmeGenerator/index.js
+++ GoodReadmeGenerator/index.js
@@ -2,97 +2,72 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const generateMarkdown = require("generateMarkdown.js");
+const api = require("api.js");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
-promptUser()
-  .then(function(readme) {
-    const html = generateReadme(readme);
+const questions = [
+  {
+    type: "input",
+    name: "userName",
+    message: "What is your github username?"
+  },
+  {
+    type: "input",
+    name: "userEmail",
+    message: "What is your email address?"
+  },
+  {
+    type: "input",
+    name: "projectName",
+    message: "What is the name of your project (repository)?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Describe your project."
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Describe how to install your project?"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How do you use this project?"
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "What type of license does this project use?"
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "How is it tested?"
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Lastly, how can someone contribute?"
+  }
+];
 
-    return writeFileAsync("output/README.md", html);
-  })
-  .then(function() {
-    console.log("Successfully created your README.md file!");
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
-
-function promptUser() {
-  return inquirer.prompt([ 
-    {
-      type: "input",
-      name: "userName",
-      message: "What is your github username?"
-    },
-    {
-      type: "input",
-      name: "userEmail",
-      message: "What is your email address?"
-    },
-    {
-      type: "input",
-      name: "projectName",
-      message: "What is the name of your project (repository)?"
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Describe your project."
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: "Describe how to install your project?"
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "How do you use this project?"
-    },
-    {
-      type: "input",
-      name: "license",
-      message: "What type of license does this project use?"
-    },
-    {
-      type: "input",
-      name: "test",
-      message: "How is it tested?"
-    },
-    {
-      type: "input",
-      name: "contributing",
-      message: "Lastly, how can someone contribute?"
-    }
-  ]);
+function writeToFile(fileName, data) {
+    const readMe = generateMarkdown(data);
+    return writeFileAsync(fileName, readMe)
 }
 
-function generateReadme(readme) {
-  return `# ${readme.projectName}
-![Most recent commit](https://img.shields.io/github/last-commit/${readme.userName}/${readme.projectName})
-## Description
-${readme.description}
-## Table of Contents
-* [Installation](##Installation)
-* [Usage](##Usage)
-* [License](##License)
-* [Contributing](##Contributing)
-* [Tests](##Tests)
-## Installation
-${readme.installation}
-## Usage
-${readme.usage}
-## License
-${readme.license}
-## Contributing
-${readme.contributing}
-## Test
-${readme.test}
-## Support
-**PHOTO GOES HERE**
-<br/>Contact me if you have any questions.
-<br/>**PROFILE NAME**
-<br/>${readme.userEmail}
-`;
-}
+function init() {
+    inquirer.prompt(questions)
+        .then((data) => {
+            api.getUser(data.username).then(function (gitRes) {
+                let newData = Object.assign(data, gitRes);
+                console.log(newData);
+                writeToFile("README.md", newData)
+            });
+        });
+};
+init();
