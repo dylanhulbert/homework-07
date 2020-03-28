0e0183002b58465e4f47adff0f766ac48d9a1b28
dylan@dylanhulbert.com <dylan@dylanhulbert.com>
2020-03-27 21:53:27 -0700

    added dependency vars

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,47 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
 
+const questions = [
+  {
+    type: "input",
+    name: "userName",
+    message: "What is your github username?"
+  },
+  {
+    type: "input",
+    name: "projectName",
+    message: "What is the name of your project (repository)?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Describe your project."
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Describe how to install your project?"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How do you use this project?"
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "What type of license does this project use?"
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "How is it tested?"
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Lastly, how can someone contribute?"
+  }
 ];
 
 function writeToFile(fileName, data) {
