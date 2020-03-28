3398411ae8920900dd3c935893dc043132418e8e
dylan@dylanhulbert.com <dylan@dylanhulbert.com>
2020-03-27 22:08:17 -0700

    updaed with functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,10 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+const api = require('./utils/api.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// the questions collected into a string
 const questions = [
   {
     type: "input",
@@ -44,11 +48,22 @@ const questions = [
   }
 ];
 
+// creates the readMe var
 function writeToFile(fileName, data) {
+    const readMe = generateMarkdown(data);
+    return writeFileAsync(fileName, readMe)
 }
 
+// this function prompts the user in the order of the question set
 function init() {
-
-}
+    inquirer.prompt(questions)
+        .then((data) => {
+            api.getUser(data.username).then(function (gitRes) {
+                let newData = Object.assign(data, gitRes);
+                console.log(newData);
+                writeToFile("README.md", newData)
+            });
+        });
+};
 
 init();
